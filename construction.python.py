d851e676ef286a2d0eef063e5de4752dc690356e
srirjjala <157482113+srirjjala@users.noreply.github.com>
2024-08-21 05:50:49 +0530

    Add files via upload

---
+++ construction.python.py
@@ -0,0 +1,22 @@
+print("construction field")
+print(" 1: sand")
+print("2 : steel")
+print(" 3: cement")
+n=int(input("enter the value of n"))
+if(n==1):
+    t=int(input("enter the type of sand"))
+if(t ==1):
+    print("sand/cost: 120")
+if(t ==2):
+    print("sand/cost :130")
+if(t ==3):
+   print("sand/cost :150")
+   if(n==2):
+       r=int(input("enter the qulity of steel"))
+if(r ==1):
+    print("stee1/ cost:123")
+if(r ==2):
+    print("steel/cost :150")
+if(r ==3):
+    print("steel/cost :125")
+    
